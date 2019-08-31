2f51d08d063f231d97fc6770815f809d5e524d4c
lixiaonan_3278@sina.com <lixiaonan_3278@sina.com>
2019-08-31 13:44:24 +0800

    创建项目

---
+++ src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div>
+    <div ><h1>登录页面</h1></div>
+    <input v-model="loginInfoVo.username" placeholder="username">
+    <input v-model="loginInfoVo.password" placeholder="password">
+    <button v-on:click="login">登录</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        loginInfoVo: {username: '', password: ''},
+        responseResult: []
+      }
+    },
+
+    methods: {
+      login() {
+
+        this.$axios
+          .post('/zuul_client/main/login', {
+            username: this.loginInfoVo.username,
+            password: this.loginInfoVo.password
+          })
+          .then(successResponse => {
+            this.responseResult = JSON.stringify(successResponse.data)
+            alert('登录成功');
+          })
+          .catch(failResponse => {})
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div><a href="#">服务A主页</a></div>
+    <div><a href="#">服务B主页</a></div>
+    <div><router-link to="/Login">登录页</router-link></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style >
+
+</style>
